f0905ed17e5d24efd47ec225a29d91c882c4cdb6
gadepall <gadepall@gmail.com>
2024-08-18 03:46:44 +0530

    Add existing files

--- codes/book/ellipse_test.py
+++ codes/book/ellipse_test.py
@@ -26,72 +26,64 @@ import shlex
 fig = plt.figure()
 ax = fig.add_subplot(111, aspect='equal')
 len = 100
-y = np.linspace(-2,2,len)
+y = np.linspace(-5,5,len)
 
+e = np.sqrt(3)/2
 #Ellipse parameters
-V = np.array(([-1/3,0],[0,1]))
+A = np.array(([3,2])).reshape(-1,1)
+B = np.array(([1,6])).reshape(-1,1)
+V = np.array(([1-e**2,0],[0,1]))
 u = np.array(([0,0])).reshape(-1,1)
-f = 4
+f = -10
 n,c,F,O,lam,P,e = conic_param(V,u,f)
 ab = ellipse_param(V,u,f)
 
-#Generating the Standard Hyperbola
-x = hyper_gen(y)
-ParamMatrix = np.diag(ab)
-print(e)
-
+#Eigenvalues and eigenvectors
+print(lam, P)
+xStandard= ellipse_gen(ab[0],ab[1])
 
 #Directrix
-k1 = -2
-k2 = 2
+k1 = -1
+k2 = 1
 
 #Latus rectum
 cl = (n.T@F).flatten()
 
-#print(c)
+#Affine conic generation
+Of = O.flatten()
+P = rotmat(np.pi/2)
 #Generating lines
+x_A = P@line_norm(n,c[0],k1,k2)+ Of[:,np.newaxis]#directrix
+x_B = P@line_norm(n,cl[0],k1,k2)+ Of[:,np.newaxis]#latus rectum
+x_C = P@line_norm(n,c[1],k1,k2)+ Of[:,np.newaxis]#directrix
+x_D = P@line_norm(n,cl[1],k1,k2)+ Of[:,np.newaxis]#latus rectum
+'''
 x_A = line_norm(n,c[0],k1,k2)#directrix
 x_B = line_norm(n,cl[0],k1,k2)#latus rectum
 x_C = line_norm(n,c[1],k1,k2)#directrix
 x_D = line_norm(n,cl[1],k1,k2)#latus rectum
-
-#Affine conic generation
-Of = O.flatten()
-
-xStandardHyperLeft = np.block([[-x],[y]])
-xStandardHyperRight= np.block([[x],[y]])
-
-
-#Generating the eigen hyperbola
-xeigenHyperLeft = ParamMatrix@xStandardHyperLeft
-xeigenHyperRight = ParamMatrix@xStandardHyperRight
-
-#Generating the actual hyperbola
-xActualHyperLeft = P@ParamMatrix@xStandardHyperLeft+Of[:,np.newaxis]
-xActualHyperRight = P@ParamMatrix@xStandardHyperRight+Of[:,np.newaxis]
-
+'''
+xActual = P@xStandard + Of[:,np.newaxis]
+#xActual = xStandard 
 
 #plotting
-#Plotting the actual hyperbola
-plt.plot(xActualHyperLeft[0,:],xActualHyperLeft[1,:],label='hyperbola',color='r')
-plt.plot(xActualHyperRight[0,:],xActualHyperRight[1,:],color='r')
-#plt.plot(xActual[0,:],xActual[1,:],label='Ellipse')
-plt.plot(x_A[0,:],x_A[1,:],label='Directrix')
-plt.plot(x_B[0,:],x_B[1,:],label='Latus Rectum')
-plt.plot(x_C[0,:],x_C[1,:])
-plt.plot(x_D[0,:],x_D[1,:])
+plt.plot(xActual[0,:],xActual[1,:],label='Ellipse')
+#plt.plot(x_A[0,:],x_A[1,:],label='Directrix')
+#plt.plot(x_B[0,:],x_B[1,:],label='Latus Rectum')
+#plt.plot(x_C[0,:],x_C[1,:])
+#plt.plot(x_D[0,:],x_D[1,:])
 #
 colors = np.arange(1,4)
 #Labeling the coordinates
-tri_coords = np.block([O,F])
+tri_coords = np.block([O,A,B])
 plt.scatter(tri_coords[0,:], tri_coords[1,:], c=colors)
-vert_labels = ['$\mathbf{O}$','$\mathbf{F}_1$','$\mathbf{F}_2$']
+vert_labels = ['$\mathbf{O}$','$\mathbf{A}$','$\mathbf{B}$']
 for i, txt in enumerate(vert_labels):
-    plt.annotate(txt, # this is the text
-#    plt.annotate(f'{txt}\n({tri_coords[0,i]:.2f}, {tri_coords[1,i]:.2f})',
+#    plt.annotate(txt, # this is the text
+    plt.annotate(f'{txt}\n({tri_coords[0,i]:.0f}, {tri_coords[1,i]:.0f})',
                  (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
                  textcoords="offset points", # how to position the text
-                 xytext=(-10,5), # distance from text to points (x,y)
+                 xytext=(0,5), # distance from text to points (x,y)
                  ha='center') # horizontal alignment can be left, right or center
 
 # use set_position
@@ -105,15 +97,15 @@ ax.spines['left'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 ax.spines['bottom'].set_visible(False)
-'''
 plt.xlabel('$x$')
 plt.ylabel('$y$')
+'''
 plt.legend(loc='best')
 plt.grid() # minor
 plt.axis('equal')
 
 #if using termux
-plt.savefig('chapters//11/11/4/13/figs/fig.pdf')
-subprocess.run(shlex.split("termux-open chapters//11/11/4/13/figs/fig.pdf"))
+plt.savefig('chapters/11/11/3/19/figs/fig.pdf')
+subprocess.run(shlex.split("termux-open chapters/11/11/3/19/figs/fig.pdf"))
 #else
 #plt.show()
--- codes/book/hyper-test.py
+++ codes/book/hyper-test.py
@@ -26,54 +26,57 @@ import shlex
 fig = plt.figure()
 ax = fig.add_subplot(111, aspect='equal')
 len = 100
-y = np.linspace(-5,5,len)
+y = np.linspace(-2,2,len)
 
-#Ellipse parameters
-V = np.array(([25,0],[0,4]))
-u = np.array(([0,0])).reshape(-1,1)
-f = -4*25
-n,c,F,O,lam,P = conic_param(V,u,f)
+#Conic parameters
+e = 4/3
+P1 =  7*e1
+P2 =  -7*e1
+V = np.array(([1-e**2,0],[0,1]))
+u = (P1+P2)/2
+f = -P1.T@V@P1
+print(f)
+n,c,F,O,lam,P,e = conic_param(V,u,f)
 ab = ellipse_param(V,u,f)
 
-#Eigenvalues and eigenvectors
-print(lam, P)
-xStandard= ellipse_gen(ab[0],ab[1])
+#Generating the Standard Hyperbola
+x = hyper_gen(y)
+ParamMatrix = np.diag(ab)
+print(e)
 
-#Directrix
-k1 = -1
-k2 = 1
 
-#Latus rectum
-cl = (n.T@F).flatten()
-
-#Generating lines
-x_A = line_norm(n,c[0],k1,k2)#directrix
-x_B = line_norm(n,cl[0],k1,k2)#latus rectum
-x_C = line_norm(n,c[1],k1,k2)#directrix
-x_D = line_norm(n,cl[1],k1,k2)#latus rectum
 #Affine conic generation
 Of = O.flatten()
-#xActual = P@xStandard + Of[:,np.newaxis]
-xActual = xStandard 
+
+xStandardHyperLeft = np.block([[-x],[y]])
+xStandardHyperRight= np.block([[x],[y]])
+
+
+#Generating the eigen hyperbola
+xeigenHyperLeft = ParamMatrix@xStandardHyperLeft
+xeigenHyperRight = ParamMatrix@xStandardHyperRight
+
+#Generating the actual hyperbola
+xActualHyperLeft = P@ParamMatrix@xStandardHyperLeft+Of[:,np.newaxis]
+xActualHyperRight = P@ParamMatrix@xStandardHyperRight+Of[:,np.newaxis]
+
 
 #plotting
-plt.plot(xActual[0,:],xActual[1,:],label='Ellipse')
-plt.plot(x_A[0,:],x_A[1,:],label='Directrix')
-plt.plot(x_B[0,:],x_B[1,:],label='Latus Rectum')
-plt.plot(x_C[0,:],x_C[1,:])
-plt.plot(x_D[0,:],x_D[1,:])
+#Plotting the actual hyperbola
+plt.plot(xActualHyperLeft[0,:],xActualHyperLeft[1,:],label='hyperbola',color='r')
+plt.plot(xActualHyperRight[0,:],xActualHyperRight[1,:],color='r')
 #
 colors = np.arange(1,4)
 #Labeling the coordinates
-tri_coords = np.block([O,F])
+tri_coords = np.block([O,P1,P2])
 plt.scatter(tri_coords[0,:], tri_coords[1,:], c=colors)
-vert_labels = ['$\mathbf{O}$','$\mathbf{F}_1$','$\mathbf{F}_2$']
+vert_labels = ['$\mathbf{O}$','$\mathbf{P}_1$','$\mathbf{P}_2$']
 for i, txt in enumerate(vert_labels):
-#    plt.annotate(txt, # this is the text
-    plt.annotate(f'{txt}\n({tri_coords[0,i]:.2f}, {tri_coords[1,i]:.2f})',
+    plt.annotate(txt, # this is the text
+#    plt.annotate(f'{txt}\n({tri_coords[0,i]:.2f}, {tri_coords[1,i]:.2f})',
                  (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
                  textcoords="offset points", # how to position the text
-                 xytext=(-20,5), # distance from text to points (x,y)
+                 xytext=(-10,5), # distance from text to points (x,y)
                  ha='center') # horizontal alignment can be left, right or center
 
 # use set_position
@@ -87,15 +90,15 @@ ax.spines['left'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 ax.spines['bottom'].set_visible(False)
+'''
 plt.xlabel('$x$')
 plt.ylabel('$y$')
-'''
 plt.legend(loc='best')
 plt.grid() # minor
 plt.axis('equal')
 
 #if using termux
-plt.savefig('chapters/11/11/3/2/figs/fig.pdf')
-subprocess.run(shlex.split("termux-open chapters/11/11/3/2/figs/fig.pdf"))
+plt.savefig('chapters//11/11/4/14/figs/fig.pdf')
+subprocess.run(shlex.split("termux-open chapters//11/11/4/14/figs/fig.pdf"))
 #else
 #plt.show()
